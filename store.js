d6bd2b1319022e1c325c16ecf7f7d354fd490806
Glaucia Rapozo <97680820+GORSFox@users.noreply.github.com>
2022-09-22 12:04:32 -0300

    Inserindo arquivos

---
+++ store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose} from 'redux';
+import rootRedux from './reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
+
+
+export default function configureStore(initialState){
+    return createStore(
+        rootRedux,
+        initialState,
+        compose(
+            applyMiddleware(thunk, reduxImmutableStateInvariant()),
+            window.devToolsExtention ? window.devToolsExtention() : f => f
+        )
+    )
+    
+};
